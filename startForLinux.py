a4e3363c56f5ca09ad1425953e246aee59a8e71b
Lihuatx <1902561088@qq.com>
2024-03-17 16:53:52 +0800

    添加Linux环境下的执行脚本

--- startForLinux.py
+++ startForLinux.py
@@ -12,7 +12,7 @@ subprocess.run(['go', 'build', '-o', 'app'])
 print("Waiting for build to finish...")
 time.sleep(1)
 
-subprocess.run(['tmux', 'new-session', '-d', '-s', 'mySession'])
+subprocess.run(['tmux', 'new-session', '-d', '-s', 'myPBFT'])
 
 # 确保你的app文件有执行权限
 commands = [
@@ -31,19 +31,41 @@ commands = [
 ]
 
 # 遍历命令和参数，然后在新的终端窗口中执行
-for exe, arg1, arg2 in commands:
-    tmux_command = f"tmux send-keys -t mySession '{exe} {arg1} {arg2}' C-m"
+# 为每个命令创建新的 Tmux 窗口，并在该窗口中执行命令
+index = 0
+for index, (exe, arg1, arg2) in enumerate(commands):
+    # 为每个进程创建新窗口，窗口名为 "app-Nx"
+    window_name = f"app-{arg1}"
+    subprocess.run(['tmux', 'new-window', '-t', f'myPBFT:{index + 1}', '-n', window_name])
+    time.sleep(1)
+
+    # 构建在新窗口中执行的命令
+    tmux_command = f"tmux send-keys -t myPBFT:{index + 1} '{exe} {arg1} {arg2}' C-m"
     subprocess.run(['bash', '-c', tmux_command])
+
 
 time.sleep(2)
 
 # 定义要执行的curl命令来代替PowerShell命令
 curl_commands = [
-    "curl -X POST -H 'Content-Type: application/json' -d '{\"clientID\":\"ahnhwi\",\"operation\":\"SendMes1\",\"timestamp\":859381532}' http://localhost:1111/req",
-    "curl -X POST -H 'Content-Type: application/json' -d '{\"clientID\":\"ahnhwi\",\"operation\":\"SendMes2\",\"timestamp\":859381532}' http://localhost:1116/req",
-    "curl -X POST -H 'Content-Type: application/json' -d '{\"clientID\":\"ahnhwi\",\"operation\":\"GetMyName\",\"timestamp\":859381532}' http://localhost:1121/req"
+    "curl -H 'Content-Type: application/json' -X POST -d '{\"clientID\":\"ahnhwi\",\"operation\":\"SendMsg1\",\"timestamp\":859381532}' http://localhost:1111/req",
+    "curl -H 'Content-Type: application/json' -X POST -d '{\"clientID\":\"ahnhwi\",\"operation\":\"SendMsg2\",\"timestamp\":859381532}' http://localhost:1116/req",
+    "curl -H 'Content-Type: application/json' -X POST -d '{\"clientID\":\"ahnhwi\",\"operation\":\"SendMsg3\",\"timestamp\":859381532}' http://localhost:1121/req",
 ]
 
+# 在 Tmux 会话中添加一个新窗口，用于执行 curl 命令
+curl_window_name = "curl-commands"
+subprocess.run(['tmux', 'new-window', '-t', 'myPBFT', '-n', curl_window_name])
+
+# 等待一段时间确保窗口创建完成
+time.sleep(1)
+
+# 获取刚创建的 curl 命令窗口的索引
+# 假设这是紧接着之前创建的窗口，其索引为 len(commands) + 1
+curl_window_index = len(commands) + 1
+
+# 在新窗口中执行 curl 命令
 for curl_command in curl_commands:
-    tmux_command = f"tmux send-keys -t mySession '{curl_command}' C-m"
+    # 构建 Tmux 命令以在新窗口中执行 curl 命令
+    tmux_command = f"tmux send-keys -t myPBFT:{curl_window_index} '{curl_command}' C-m"
     subprocess.run(['bash', '-c', tmux_command])
