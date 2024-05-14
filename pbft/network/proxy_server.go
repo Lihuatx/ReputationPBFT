4a69c06e7c05c02ad2dc698e0ef4840c03288453
Lihuatx <1902561088@qq.com>
2024-05-14 20:21:35 +0800

    正在修改viewchange，备份一下

--- pbft/network/proxy_server.go
+++ pbft/network/proxy_server.go
@@ -11,17 +11,17 @@ import (
 	"time"
 )
 
-//func init() {
-//	// 配置 http.DefaultTransport
-//	http.DefaultTransport.(*http.Transport).DialContext = (&net.Dialer{
-//		Timeout:   30 * time.Second,
-//		KeepAlive: 30 * time.Second,
-//	}).DialContext
-//	http.DefaultTransport.(*http.Transport).MaxIdleConns = 200
-//	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 100
-//	http.DefaultTransport.(*http.Transport).IdleConnTimeout = 90 * time.Second
-//	http.DefaultTransport.(*http.Transport).TLSHandshakeTimeout = 10 * time.Second
-//}
+func init() {
+	// 配置 http.DefaultTransport
+	http.DefaultTransport.(*http.Transport).DialContext = (&net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}).DialContext
+	http.DefaultTransport.(*http.Transport).MaxIdleConns = 200
+	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 100
+	http.DefaultTransport.(*http.Transport).IdleConnTimeout = 90 * time.Second
+	http.DefaultTransport.(*http.Transport).TLSHandshakeTimeout = 10 * time.Second
+}
 
 type Server struct {
 	url  string
