a656727e65f098259e9452bf6469a0b4a2cad574
Lihuatx <1902561088@qq.com>
2024-05-15 20:36:20 +0800

    正在修改viewchange，备份一下

--- linuxTest.py
+++ linuxTest.py
@@ -54,10 +54,10 @@ if arg == "N":
     client_socket.connect((base_server_ips[1], 2000))
     message = "link"
     client_socket.sendall(message.encode())
-    # client_socket_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # client_socket_2.connect((base_server_ips[2], 2000))
-    # message = "link"
-    # client_socket_2.sendall(message.encode())
+    client_socket_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket_2.connect((base_server_ips[2], 2000))
+    message = "link"
+    client_socket_2.sendall(message.encode())
     # client_socket_3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # client_socket_3.connect((base_server_ips[3], 2000))
     # message = "link"
