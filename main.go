de1d74286bccea844338c225f20ad0517de8a084
Lihuatx <1902561088@qq.com>
2024-06-02 17:44:26 +0800

    正在修改viewchange，备份一下

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 
 	nodeID := os.Args[1]
 	clusterName := os.Args[2]
-	sendMsgNumber := 30
+	sendMsgNumber := 40
 	if nodeID == "client" {
 		client := network.ClientStart(clusterName)
 
